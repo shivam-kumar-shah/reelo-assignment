8011be24cfb82736f38b660b4d4de8ab37a62cf1
= <=>
2023-11-21 21:15:54 +0530

    feat: :sparkles: add question controller

---
+++ src/controllers/questionaire.ts
@@ -0,0 +1,33 @@
+import { RequestHandler, query } from "express";
+import { Questionaire } from "../models";
+import { ApiResponse } from "../types/response";
+import { QueryQuestion } from "../types/request";
+import { prepareQuestionare } from "../lib";
+
+export const getAllQuestions: RequestHandler<{}, ApiResponse> = async (
+  req,
+  res,
+  next
+) => {
+  const result = await Questionaire.Instance.get();
+  const response: ApiResponse = {
+    statusCode: 200,
+    message: "Got all questions",
+    data: result,
+  };
+  res.status(response.statusCode).send(response);
+};
+
+export const getQuestionare: RequestHandler<
+  {},
+  ApiResponse,
+  QueryQuestion
+> = async (req, res, next) => {
+  const result = prepareQuestionare(req.body);
+  const response: ApiResponse = {
+    statusCode: 200,
+    message: "Got queried questions",
+    data: result,
+  };
+  res.status(response.statusCode).send(response);
+};
