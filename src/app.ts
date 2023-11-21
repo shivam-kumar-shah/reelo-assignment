b3b75ada34ca1c7fa49afbedec075e655aaddc3b
= <=>
2023-11-21 21:17:36 +0530

    ci: :sparkles: add middlewares

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@ import express from "express";
 import { config } from "dotenv";
 import { initServer } from "./lib";
 import { questionRouter } from "./routes";
+import { errorHandler } from "./middlewares/error";
 
 config();
 
 const app = express();
+app.use(express.json());
 app.use(questionRouter);
+
+app.use(errorHandler);
 
 initServer(app);
