650913fe91b5ab61e691de8f2ae36ef1e78516cb
= <=>
2023-11-22 13:54:27 +0530

    docs: :memo: add example for questionare route

--- src/routes/questionaire.ts
+++ src/routes/questionaire.ts
@@ -8,6 +8,28 @@ const questionRouter = Router();
 
 questionRouter.get("/questions", getAllQuestions);
 
+/**
+ * Example Query for the post route - 
+ * 
+{
+  "totalMarks":100,
+  "split":[
+    {
+      "difficulty":"easy",
+      "weightageInPercentage":10
+    },
+    {
+      "difficulty":"medium",
+      "weightageInPercentage":40
+    },
+    {
+      "difficulty":"hard",
+      "weightageInPercentage":50
+    }
+  ]
+}
+*/
+
 questionRouter.post(
   "/questions",
   checkSchema(QueryQuestionSchema),
